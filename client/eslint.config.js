15544c9295accb86f669b62b16b3c36be5547fc2
Diarmuid Murphy <diarmuid.com@gmail.com>
2024-09-26 21:00:58 -0700

    did this work

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -3,7 +3,6 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
-import eslintConfigPrettier from "eslint-config-prettier";
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default tseslint.config(
