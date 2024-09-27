bb0751accb76818398bcc8a308aad55e702edfcb
Diarmuid Murphy <diarmuid.com@gmail.com>
2024-09-26 21:22:47 -0700

    added route

--- server/server.js
+++ server/server.js
@@ -1,12 +1,37 @@
 import express from "express";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import sequelize from "./config/connections.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("../client/dist"));
+
+app.get("/api/test", (req, res) => {
+  res.json({
+    message: "Hello from the server!",
+  });
 });
 
-app.listen(PORT, () =>
-  console.log(`Server is running on port http://localhost:${PORT}`),
-);
+app.get("/test", (req, res) => {
+  res.send("Hello from the server!");
+});
+
+if (process.env.NODE_ENV === "production") {
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
+  });
+}
+
+(async () => {
+  await sequelize.sync({ force: true }); // NOTE: Change to false when you have finalized your models
+  app.listen(PORT, () =>
+    console.log(`Server is running on port http://localhost:${PORT}`),
+  );
+})();
